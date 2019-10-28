dfc7ce494c396766f6841d6bd70b2f1288ead7eb
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-10-28 11:59:47 +0800

    Replaced Old API

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ let express = require('express');
 let router = express.Router();
 let checkJWT = require('../middlewares/checkJWT');
 
-router.use('/api', require('../api/index'));
+// TODO: Error pages https://github.com/expressjs/express/tree/master/examples/error-pages
+
+router.use('/api/v1', require('./apiv1'));
+router.use('/api', require('./apiv1'));
 
 router.get('/',function (req, res, next) {
     res.render('index', {title: 'Express'});
